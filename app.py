7a381727855cda14ab4436f17041e62625c3c928
gournath1 <33548052+gournath1@users.noreply.github.com>
2023-04-02 13:15:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Welcome to LangChain All purpose Q&A Bot!'
+
+os.system("bot.py")
 
 if __name__ == '__main__':
     app.run()
